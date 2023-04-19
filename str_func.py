c4a9fd5f493155ac919e5123f0bed792cd5eaa42
shlegel <sveta.shlegel@mail.ru>
2023-04-19 22:34:25 +0300

    add docstrings and new function

--- str_func.py
+++ str_func.py
@@ -1,5 +1,10 @@
 def up_string(string):
+    """Делаеи все буквы в строке заглавными"""
     new_string = string.upper()
     return new_string
 
 
+def up_first_letter(string):
+    """Делает первые буквы в строке заглавными"""
+    new_string = string.title()
+    return new_string
