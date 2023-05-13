bea25be6fa08a579cd0562f6226256e4e0385bbf
shlegel <sveta.shlegel@mail.ru>
2023-05-13 11:07:13 +0300

    new func

--- str_func.py
+++ str_func.py
@@ -8,3 +8,8 @@ def up_first_letter(string):
     """Делает первые буквы в строке заглавными"""
     new_string = string.title()
     return new_string
+
+
+def print_hello(username):
+    """Здоровается с пользователем"""
+    return f"Hello, {username}"
